2e0091a4125a655e0de2f0f9ba07c93bbac6f5e1
Alessevan <44125445+Alessevan@users.noreply.github.com>
2025-03-29 17:25:36 +0100

    Ajout logs pour les sanctions de bannissement, débanissement et time out (#2)
    * feat: add log for ban, unban and time out
    
    * fix: unnecessary non-null check removed
    
    * doc: add more doc for functions & remove typos
    
    * feat: remove string interpolation for AuditLogAction
    
    * feat: log sanctions in database
    
    * refactor: use user_fetch to get user's name
    
    * feat: add duration string conversion
    
    * feat: change warnlist to sanctionslist and display all kinds of sanctions
    
    * style: rename sanctionslist to sanctionlist
    
    * style: seconds to secondes
    
    * feat: add expected exception's type
    
    Co-authored-by: Tristan Pagezy <61625831+Triw-12@users.noreply.github.com>
    
    * style: rename to by duration in sanctionmodel's representation
    
    * style: more PEP8 compliant
    
    * refactor: build directly embeds and do not factorize code
    
    * feat: handle correctly exception when a DM is sent for a TO
    
    * feat: refine exception if needed
    
    * feat: filter audit log entries before fetching user
    
    * fix: forgot embed's d
    
    * refactor: use keyword type as variable
    
    * refactor: use discord's terminology
    
    * feat: send a DM when TO iff there is a reason
    
    * refactor: use boolean as parameter
    
    * fix: multiline reasons are now well-formated
    
    * feat: use humanize package to convert seconds in text time
    
    * docs: change send_dm to bool
    
    Co-authored-by: Tristan Pagezy <61625831+Triw-12@users.noreply.github.com>
    
    * feat: add context menu to warn members
    
    * style: normalize function calls
    
    * style: use a single line when it's possible
    
    * feat: make context menu guild only
    
    * fix: make variable local
    
    * fix: role checking failed due to missing attribute author
    
    * doc: simplify placeholder content
    
    Co-authored-by: Tristan Pagezy <61625831+Triw-12@users.noreply.github.com>
    
    * fix: ignore case of submitted content
    
    Co-authored-by: Tristan Pagezy <61625831+Triw-12@users.noreply.github.com>
    
    * chore: allow more humanize versions
    
    * fix: error when reason was None
    
    ---------
    
    Co-authored-by: Tristan Pagezy <61625831+Triw-12@users.noreply.github.com>

--- mp2i/utils/discord.py
+++ mp2i/utils/discord.py
@@ -24,6 +24,20 @@ def defer(ephemeral: bool = False):
 
     return decorator
 
+def predicate_has_any_role(guild, member, *items: str):
+    if guild is None:
+        raise NoPrivateMessage()
+
+    # ctx.guild is None doesn't narrow ctx.author to Member
+    guild = GuildWrapper(guild)
+    roles_id = {role.id for role in member.roles}
+    for item in items:
+        if (role := guild.get_role_by_qualifier(item)) is None:
+            logger.error(f"{item} role is not defined in the configuration file")
+        elif role.id in roles_id:
+            return True
+
+    raise MissingAnyRole(list(items))
 
 def has_any_role(*items: str):
     """
@@ -31,18 +45,17 @@ def has_any_role(*items: str):
     """
 
     def predicate(ctx):
-        if ctx.guild is None:
-            raise NoPrivateMessage()
+        return predicate_has_any_role(ctx.guild, ctx.author, *items)
 
-        # ctx.guild is None doesn't narrow ctx.author to Member
-        guild = GuildWrapper(ctx.guild)
-        roles_id = {role.id for role in ctx.author.roles}
-        for item in items:
-            if (role := guild.get_role_by_qualifier(item)) is None:
-                logger.error(f"{item} role is not defined in the configuration file")
-            elif role.id in roles_id:
-                return True
+    return check(predicate)
 
-        raise MissingAnyRole(list(items))
+
+def interaction_has_any_role(*items: str):
+    """
+    Decorator that check if the user has any of the specified roles.
+    """
+
+    def predicate(interaction):
+        return predicate_has_any_role(interaction.guild, interaction.user, *items)
 
     return check(predicate)
