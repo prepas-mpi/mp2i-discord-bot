277c407ff40c7997f455334913856f41fb51ca24
Alessevan <44125445+Alessevan@users.noreply.github.com>
2025-11-20 13:25:00 -0500

    refactor: externalize has_any_role predicate to use it alone

--- mp2i/utils/discord.py
+++ mp2i/utils/discord.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List
+from typing import Any, Callable, List
 
 import discord
 from discord.app_commands import MissingAnyRole, check
@@ -9,7 +9,44 @@ from mp2i.wrappers.guild import GuildWrapper
 logger: logging.Logger = logging.getLogger(__name__)
 
 
-def has_any_role(*roles):
+async def has_any_roles_predicate(
+    interaction: discord.Interaction, *roles: str
+) -> bool:
+    """
+    Predicate that check if user has one of the required roles
+
+    Parameters
+    ----------
+    ctx : Context
+        Context of an interaction
+
+    Returns
+    -------
+    bool
+        True if the user has one of the required role, False otherwise
+
+    Raises
+    ------
+    MissingAnyRole
+        Error if the check do not pass, contains a list of required roles
+    """
+    if not interaction.guild:
+        logger.warning("Using has_config_role in a non guild context.")
+        raise MissingAnyRole(list(roles))
+
+    guild: GuildWrapper = GuildWrapper(interaction.guild)
+    member: discord.Member | discord.User = interaction.user
+    if isinstance(member, discord.User):
+        logger.error("User is not member in the guild %d.", guild.id)
+        raise MissingAnyRole(list(roles))
+    needed_role: List[discord.Role] = guild.mapping_roles(list(roles))
+    for role in needed_role:
+        if role in member.roles:
+            return True
+    raise MissingAnyRole(list(roles))
+
+
+def has_any_role(*roles: str) -> Callable[[Any], Any]:
     """
     Decorator to control access to specific commands
 
@@ -24,35 +61,4 @@ def has_any_role(*roles):
         Predicate
     """
 
-    async def predicate(ctx: discord.Interaction):
-        """
-        Predicate that check if user has one of the required roles
-
-        Parameters
-        ----------
-        ctx : Context
-            Context of an interaction
-
-        Returns
-        -------
-        bool
-            True if the user has one of the required role, False otherwise
-        """
-        if not ctx.guild:
-            logger.warning("Using has_config_role in a non guild context.")
-            raise MissingAnyRole(list(roles))
-
-        guild: GuildWrapper = GuildWrapper(ctx.guild)
-        member: discord.Member | discord.User = (
-            ctx.user if isinstance(ctx, discord.Interaction) else ctx.author
-        )
-        if isinstance(member, discord.User):
-            logger.error("User is not member in the guild %d.", guild.id)
-            raise MissingAnyRole(list(roles))
-        needed_role: List[discord.Role] = guild.mapping_roles(list(roles))
-        for role in needed_role:
-            if role in member.roles:
-                return True
-        raise MissingAnyRole(list(roles))
-
-    return check(predicate)
+    return check(lambda interaction: has_any_roles_predicate(interaction, *roles))
